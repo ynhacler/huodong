fbc8ac996dcd8d9a61241fc261e747a158e6a1d9
git <zzh1989@126.com>
2017-03-09 22:03:57 +0800

    v

--- getpaihang.php
+++ getpaihang.php
@@ -3,6 +3,12 @@ require_once 'getcode.php';
 require_once 'common.php';
 header("Content-Type: text/html;charset=utf-8"); 
 
+$a['a'] = 12;
+$a['b'] = 12;
+$a['c'] = 12;
+var_dump(json_encode($a));
+exit;
+
 if(is_array($_GET) && count($_GET)>0 && isset($_GET["next"]) && isset($_GET["jiang"]))//先判断是否通过get传值了
 {
 	$next_num = (int)($_GET["next"])-1;
