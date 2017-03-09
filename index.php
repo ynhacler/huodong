10d4a42f13c2e7210f100b840a18df155208e2b6
git <zzh1989@126.com>
2017-03-09 18:36:49 +0800

    ddd

--- index.php
+++ index.php
@@ -4,10 +4,22 @@ $zzh = new weixinController();
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title></title>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<meta name="format-detection"content="telephone=no, email=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" /><!-- 删除苹果默认的工具栏和菜单栏 -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" /><!-- 设置苹果工具栏颜色 -->
+	<meta name="format-detection" content="telphone=no, email=no" /><!-- 忽略页面中的数字识别为电话，忽略email识别 -->
+	<link rel="stylesheet" href="/pub/style.css">
+	<script type="text/javascript" src="/pub/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="/pub/rem.js"></script>
+  <title>首页</title>
 </head>
 <body>
 	<a href="http://www.uhit.me/wd.php">礼物设置</a><br/>
