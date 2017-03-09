72fb8cfe16e72523c9e515cd8ad74482760dc894
git <zzh1989@126.com>
2017-03-09 21:16:28 +0800

    sss

--- getpaihang.php
+++ getpaihang.php
@@ -5,10 +5,11 @@ header("Content-Type: text/html;charset=utf-8");
 
 if(is_array($_GET) && count($_GET)>0 && isset($_GET["next"]) && isset($_GET["jiang"]))//先判断是否通过get传值了
 {
-	$next_num = (int)($_GET["next"]);
+	$next_num = (int)($_GET["next"])-1;
 	$jiang = $_GET["jiang"];
 
-	$sqlz = "select b.openid ,b.nickname ,b.headimgurl,a.praised_num from event_user a,wx_user b where b.id=a.wx_id and a.gift_id='{$jiang}' order by a.praised_num desc limit {$next_num}-1,5;";
+	$sqlz = "select b.openid ,b.nickname ,b.headimgurl,a.praised_num from event_user a,wx_user b where b.id=a.wx_id and a.gift_id='{$jiang}' order by a.praised_num desc limit {$next_num},5;";
+	echo $sqlz;
 	$re = select_DB_2($sqlz);
 	foreach ($re as $key => $value) {
 	}
@@ -19,4 +20,3 @@ if(is_array($_GET) && count($_GET)>0 && isset($_GET["next"]) && isset($_GET["jia
 }
 
 ?>
-
