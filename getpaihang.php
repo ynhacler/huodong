fbca27f3a2155c4a1ea102d80c91903ea3860ab1
git <zzh1989@126.com>
2017-03-09 22:10:26 +0800

    x

--- getpaihang.php
+++ getpaihang.php
@@ -3,13 +3,6 @@ require_once 'getcode.php';
 require_once 'common.php';
 header("Content-Type: text/html;charset=utf-8"); 
 
-$a['a'] = 12;
-$a['b'] = 12;
-$a['c'] = 12;
-var_dump($a);
-var_dump(json_encode($a));
-exit;
-
 if(is_array($_GET) && count($_GET)>0 && isset($_GET["next"]) && isset($_GET["jiang"]))//先判断是否通过get传值了
 {
 	$next_num = (int)($_GET["next"])-1;
@@ -20,12 +13,11 @@ if(is_array($_GET) && count($_GET)>0 && isset($_GET["next"]) && isset($_GET["jia
 	$re = select_DB_2($sqlz);
 	$zz['meg'] = 'OK';
 	$zz['info'] = $re;
-	var_dump(json_encode($zz));
-	exit;
+
 }else{
 	$zz['meg'] = 'ERROR';
-	var_dump(json_encode($zz));
-	exit;
 }
+	echo json_encode($zz);
+	exit;
 
 ?>
