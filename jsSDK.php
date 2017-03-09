079acd7fe69a4448e5e556c1a96f1b004cbbfb07
git <zzh1989@126.com>
2017-03-10 01:10:46 +0800

    d

--- jsSDK.php
+++ jsSDK.php
@@ -10,7 +10,7 @@ class JSSDK {
 
   public function getSignPackage() {
     $jsapiTicket = $this->getJsApiTicket();
-    $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     $timestamp = time();
     $nonceStr = $this->createNonceStr();
 
